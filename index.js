93c1fdbfeb6be28e3b1fa6ce0cc28f38d484e20a
Sushant Singh <sushant22007@gmail.com>
2025-12-09 16:46:17 +0530

    fist commit of rest-appi practincing question and revise node.js concept

---
+++ index.js
@@ -0,0 +1,29 @@
+import express, { response } from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+//challenge 1.
+
+app.get('/user', (req, res) => {
+    res.json([
+        {id:1, name: "sushant"},
+        {id:2, name:"rounak"}
+    ])
+})
+
+//challenge 2.
+
+let user = [];
+
+app.post('/post-user', (req, res) => {
+    user.push(req.body)
+    res.send("user Added")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
